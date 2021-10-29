ea982605ed2987780c638d186017effca715ac52
AKutanova <anna-vlk@yandex.ru>
2021-10-29 18:38:15 +0300

    дошла до блока контактов

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu');
+      menu = document.querySelector('.menu__block'),
+      overlay = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-    hamburger.classList.add('open');
+    menu.classList.toggle('active');
+    hamburger.classList.toggle('open');
+    overlay.classList.toggle('active');
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+    hamburger.classList.remove('open');
+    overlay.classList.remove('active');
 });

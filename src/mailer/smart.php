0c0172df6848918a95408cb6d8e6893491ffaf6e
anna.kutanova <anna.kutanova@kelnik.ru>
2022-11-24 13:33:16 +0300

    контентные и фото изменения

---
+++ src/mailer/smart.php
@@ -0,0 +1,43 @@
+<?php 
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+//$email = $_POST['email'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'a.kutanova.msk@gmail.com';                 // Наш логин
+$mail->Password = 'Dutik7278';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('a.kutanova.msk@gmail.com', 'МеталлСалон');   // От кого письмо 
+$mail->addAddress('anna-vlk@yandex.ru');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Просьба перезвонить';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone;
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
